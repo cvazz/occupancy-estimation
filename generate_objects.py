4d863c679ab8942137890a750caf008647b1504f
Sebastian Bielfeldt <50203100+cvazz@users.noreply.github.com>
2025-05-28 14:09:25 +0200

    Sporadic docstrings

--- generate_objects.py
+++ generate_objects.py
@@ -5,6 +5,10 @@ from scipy import ndimage
 from scipy import stats
 import sys, os
 
+# get fig folder
+from inspect import getsourcefile
+from os.path import abspath, dirname
+
 try:
     import gemmi 
 except ImportError:
@@ -21,8 +25,32 @@ try:
 except ImportError:
     print("meteor not available")
     
+############################## Folder Magic ######################################    
+
+def get_base_folder():
+    """returns current folder location (of this file)
+
+    Returns:
+        str: location of folder
+    """
+    return dirname(abspath(getsourcefile(lambda:0)))
+
+def get_fig_folders():
+    """retuns folders for figures
+
+    Returns:
+        list: strings of folders to save to
+    """
+
+    base_folder = get_base_folder()
+    return [
+        base_folder + "/plots/",
+        base_folder + "/../tex/figs/",
+    ]
+
 ############################## 2d Objects ######################################    
 def make_objs(
+
     nx=256,
     ny=256,
     n_pos=600,
@@ -33,6 +61,42 @@ def make_objs(
     blur_by=2,
     seed=0,
 ):
+    """
+    Generate a pair of similar objects with a small region of differences.
+
+    This function creates two 2D arrays (objects) with random dots, where the second object
+    has a specific region where dots are shifted by a small delta compared to the first object.
+    Both objects can optionally be blurred with a Gaussian filter.
+
+    Parameters
+    ----------
+    nx : int, optional
+        Width of the output arrays, by default 256
+    ny : int, optional
+        Height of the output arrays, by default 256
+    n_pos : int, optional
+        Number of random positions (dots) to generate, by default 600
+    dx : int, optional
+        Width of the region where objects will differ, by default 50
+    dy : int, optional
+        Height of the region where objects will differ, by default 50
+    delta_x : int, optional
+        Horizontal shift applied to dots in the difference region for obj1, by default 3
+    delta_y : int, optional
+        Vertical shift applied to dots in the difference region for obj1, by default 3
+    blur_by : int, optional
+        Standard deviation for Gaussian kernel used for blurring, by default 2.
+        If 0, no blurring is applied.
+    seed : int, optional
+        Random seed for reproducibility, by default 0
+
+    Returns
+    -------
+    tuple of numpy.ndarray
+        A tuple containing (obj1, obj0), where:
+        - obj1: Array with shifted dots in the difference region
+        - obj0: Initial array with random dots
+    """
 
     # Initialize arrays
     obj0 = np.zeros((nx, ny))
@@ -144,6 +208,18 @@ def generate_obj_cistrans(imagetype, mean_value_offset=0):
 
 
 def generate_obj(imagetype, kwargs={}):
+    """
+    selector between different generator functions
+    
+    Parameters
+    ----------
+    imagetype : str
+        defines type of generated object
+
+    Returns 
+    ----------
+    values    
+    """
     if imagetype == "2d":
         alpha = 0.3 if not "alpha" in kwargs.keys() else kwargs["alpha"]
         obj1, obj0 = make_objs(
@@ -169,49 +245,101 @@ def generate_obj(imagetype, kwargs={}):
     
 
 def generate_obj_v2(imagetype, kwargs={}):
-        mean_value_offset = (
-            0.3
-            if not "mean_value_offset" in kwargs.keys()
-            else kwargs["mean_value_offset"]
-        )
-        if "offset" in imagetype:
-            mean_value_offset= 90_000 
-            imagevariant = imagetype[:-7]
+    """generate objects
+
+    Args:
+        imagetype (str): describes which objects to generate
+        kwargs (dict, optional): _description_. Defaults to {}.
+
+    Returns:
+        _type_: _description_
+    """
+
+    mean_value_offset = (
+        0.3
+        if not "mean_value_offset" in kwargs.keys()
+        else kwargs["mean_value_offset"]
+    )
+    if "offset" in imagetype:
+        mean_value_offset= 90_000 
+        imagevariant = imagetype[:-7]
+    else:
+        mean_value_offset = 0
+        imagevariant = imagetype
+    return generate_obj_cistrans(imagevariant, mean_value_offset)
+
+def struc2realspace(struc, hs_limit, noise_level):
+    """
+    Convert a molecular structure to real space electron density map with optional noise.
+
+    This function calculates structure factors from a given molecular structure,
+    adds Gaussian noise to the amplitudes, and performs an inverse Fourier transform
+    to obtain a real space electron density map.
+
+    Parameters
+    ----------
+    struc : gemmi.Structure or similar
+        The molecular structure to convert to real space.
+        
+    hs_limit : float
+        High resolution limit (in Angstroms) for the structure factor calculation.
+        
+    noise_level : float
+        Scale factor for the noise to be added to the structure factor amplitudes.
+        The noise is drawn from a normal distribution with mean 0 and
+        standard deviation equal to noise_level * |F|.
+
+    Returns
+    -------
+    numpy.ndarray
+        Real space electron density map as a 3D numpy array.
+    """
+    map_vals = meteor.sfcalc.gemmi_structure_to_calculated_map(struc, high_resolution_limit=hs_limit)
+    struc_vals=rs.DataSet(map_vals)
+    noise = np.random.normal(loc=0.0, scale=noise_level * np.abs(struc_vals["F"]))
+    struc_vals["F"] = struc_vals["F"] + noise
+    struc_vals["sf"] = struc_vals.to_structurefactor("F","PHI")
+    struc_vals_grid = struc_vals.to_reciprocal_grid("sf")
+    struc_vals_real = np.fft.ifftn(struc_vals_grid).real
+    return struc_vals_real
+
+
+def overwrite_occupancy(struc, new_occ):
+    target_occ = 0.27
+    for x in struc[0].all():
+        if np.round(x.atom.occ,2)==  target_occ:
+            x.atom.occ=new_occ
+        elif np.round(x.atom.occ,2) == 1-target_occ:
+            x.atom.occ=1-new_occ
         else:
-            mean_value_offset = 0
-            imagevariant = imagetype
-        return generate_obj_cistrans(imagevariant, mean_value_offset)
+            pass
+    return struc
     
 def generate_obj_cistrans_v2(occupancy, noise_level, no_negs = False):
-    identifier = f"ct_occ_{occupancy*100:.0}_noise_{noise_level*100:.0}"
+    hs_limit = 1.6
+    identifier = f"ct_occ_{occupancy*100:.0f}_noise_{noise_level*100:.0f}"
     dataloc = "../synthetic_cistrans/"
-    name_dark = "trans_sf.mtz"
-    name_light = "100ps_sf.mtz"
-    alpha = 0.27
+    pdbname_dark = "trans.pdb"
+    pdbname_light = "100ps.pdb"
 
-    ds_light = rs.read_mtz(dataloc + name_light)
-    ds_dark = rs.read_mtz(dataloc + name_dark)
+    struc_dark = gemmi.read_structure(dataloc + pdbname_dark)
+    struc_light = gemmi.read_structure(dataloc + pdbname_light)
 
-    ds_light["sf"] = ds_light.to_structurefactor("FC", "PHIC")
-    ds_dark["sf"] = ds_dark.to_structurefactor("FC", "PHIC")
+    struc_light = overwrite_occupancy(struc_light, occupancy)
 
-    f_light = ds_light.to_reciprocal_grid("sf")
-    f_dark = ds_dark.to_reciprocal_grid("sf")
-    delta_fa_abs = np.abs(f_light) - np.abs(f_dark)
+    obj0 = struc2realspace(struc_dark, hs_limit, noise_level)
+    obj1 = struc2realspace(struc_light, hs_limit, noise_level)
 
     if no_negs:
-        obj1 = np.real(np.fft.ifftn(f_light))
-        obj0 = np.real(np.fft.ifftn(f_dark))
         minobj1 = np.min(obj1)
         if minobj1<0:
             obj0-=minobj1
             obj1-=minobj1
-        f_dark = np.fft.fftn(obj0)
-        f_light = np.fft.fftn(obj1)
 
-    else:
-        obj1 = np.real(np.fft.ifftn(f_light))
-        obj0 = np.real(np.fft.ifftn(f_dark))
+    f_dark = np.fft.fftn(obj0)
+    f_light = np.fft.fftn(obj1)
 
-    return obj0, obj1, f_dark, f_light, delta_fa_abs, alpha
+    delta_fa_abs = np.abs(f_light) - np.abs(f_dark)
+
+    return obj0, obj1, f_dark, f_light, delta_fa_abs, identifier
     
