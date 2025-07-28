e90a2f98782be9a95280b3d031de642aa89b3c47
Sebastian Bielfeldt <50203100+cvazz@users.noreply.github.com>
2025-07-28 16:27:56 +0200

    better intersections& smarter blob selection and rewriting finding_largest_blobs

--- meteorize.py
+++ meteorize.py
@@ -21,6 +21,7 @@ from meteor import rsmap
 from scipy.ndimage import label, generate_binary_structure
 
 import logging
+
 logger = logging.getLogger(__name__)
 
 ################################################################################
@@ -45,10 +46,19 @@ def scale_structure_factors(ds_dark, ds_light, dark_columns, light_columns):
     ds_scaleit = rs.DataSet(cell=ds_dark.cell, spacegroup=ds_dark.spacegroup)
     logger.info(f"Dark columns: {ds_dark.columns}")
     logger.info(f"Dark amplitude column: {dark_f}")
-    ds_scaleit[out_columns["dark_f"]] = ds_dark[dark_f]
-    ds_scaleit[out_columns["dark_sig"]] = ds_dark[dark_sig]
-    ds_scaleit[out_columns["light_f"]] = ds_light[light_f]
-    ds_scaleit[out_columns["light_sig"]] = ds_light[light_sig]
+    try:
+        ds_scaleit[out_columns["dark_f"]] = ds_dark[dark_f]
+        ds_scaleit[out_columns["dark_sig"]] = ds_dark[dark_sig]
+        ds_scaleit[out_columns["light_f"]] = ds_light[light_f]
+        ds_scaleit[out_columns["light_sig"]] = ds_light[light_sig]
+    except KeyError:
+
+        logger.error(ds_dark.columns)
+        logger.error(ds_light.columns)
+        logger.error(dark_columns)
+        logger.error(light_columns)
+        raise KeyError
+
     logger.info(f"Scaled dataset columns: {ds_scaleit.columns}")
 
     ds_scaleit = run_scaleit(ds_scaleit, None, False, columns=out_columns)
@@ -142,37 +152,54 @@ def many_negsum(
     masks: np.ndarray = None,
     detailed: bool = False,
     return_neg_sum: bool = False,
+    diffmap: rsmap.Map = None,
 ):
+
+    if len(masks) < 1:
+        logger.error(f"Error: No masks provided")
+        value_tuple = (np.nan, np.nan, np.nan, np.nan)
+        if detailed and return_neg_sum:
+            return (
+                value_tuple,
+                np.empty((arrlen, len(masks))) * np.nan,
+                np.empty(len(masks)) * np.nan,
+                np.empty(len(masks)) * np.nan,
+            )
+        elif detailed:
+            return value_tuple, (np.nan, np.nan)
+        if return_neg_sum:
+            return np.empty((arrlen, len(masks))) * np.nan
+    weight = np.empty(len(masks))
+    if diffmap is not None:
+        diffmap_density = diffmap.to_3d_numpy_map(map_sampling=map_sampling)
+        for ii, mask in enumerate(masks):
+            weight[ii] = np.sum(np.abs(diffmap_density[mask]))
+    else:
+        logger.warning(f"No Diffmap provided for Weighting Calculation")
+        for ii, mask in enumerate(masks):
+            weight[ii] = np.sum(mask) ** 2
+    # find the 1000 masks with the heaviest weight
+    max_masks = 1000
+    if len(weight) > max_masks:
+        logger.warning(
+            f"Too many masks ({len(weight)}), selecting {max_masks} with the largest diffmap contribution."
+        )
+        sorted_indices = np.argsort(weight)[-max_masks:]
+        logger.info(f"Selected masks: {sorted_indices[sorted_indices>max_masks]}")
+        # logger.info(masks.shape)
+        # masks.shape
+
+        masks = np.array(masks)[sorted_indices]
+        weight = weight[sorted_indices]
+
     n_largest = 4
     arrlen = len(rho_xtrs)
-    masklen = len(masks)
-    if masklen > 1000:
-        logger.warning(f"Warning: Many masks ({masklen}) provided, this may take a while.")
-    if masklen>3000:
-        logger.error(f"Error: Too many masks ({masklen}), please reduce the number of masks.")
-        
-        value_tuple = ( np.nan, np.nan, np.nan, np.nan)
-        if detailed and return_neg_sum:
-            return (value_tuple, 
-                    np.empty((arrlen, len(masks)))*np.nan,
-                    np.empty(len(masks))*np.nan)
-        elif detailed:
-            return value_tuple
-        if return_neg_sum:
-            return np.empty((arrlen, len(masks)))*np.nan
-
-
-    weight = np.empty(len(masks))
-    for ii, mask in enumerate(masks):
-        weight[ii] = np.sum(mask) ** 2
 
     neg_sum = np.empty((arrlen, len(masks)))
-
     for ii, density in enumerate(rho_xtrs):
         density = density.to_3d_numpy_map(map_sampling=map_sampling)
         for jj, mask in enumerate(masks):
             neg_sum[ii, jj] = np.sum(density[mask][density[mask] < 0])
-
 
     intersection_points = np.empty(len(masks))
     for jj, mask in enumerate(masks):
@@ -180,31 +207,42 @@ def many_negsum(
             extrapolation_factors, neg_sum[:, jj], n_largest
         )
         intersection_points[jj] = intersect
-    ma = np.ma.MaskedArray(intersection_points, mask=np.isnan(intersection_points))
-    intersection_average = np.ma.average(ma, weights=weight)
-    intersection_std = np.sqrt(np.cov(ma, aweights=weight))
     # calculate standard deviation with weights, masking NaNs
-    print(f"Intersection Average: {intersection_average:.2f} ± {intersection_std:.2f}")
-
-    intersection_average_inv = np.ma.average(2 / ma, weights=weight)
-    intersection_std_inv = np.sqrt(np.cov(2 / ma, aweights=weight))
-    print(
+    finite_intersections = np.isfinite(intersection_points)
+    masked_intersection = intersection_points[finite_intersections]
+    masked_weight = weight[finite_intersections]
+    intersection_average = np.ma.average(masked_intersection, weights=masked_weight)
+    intersection_std = np.sqrt(np.cov(masked_intersection, aweights=masked_weight))
+    logger.info(
+        f"Intersection Average 2: {intersection_average:.2f} ± {intersection_std:.2f}"
+    )
+    intersection_average_inv = np.ma.average(
+        2 / masked_intersection, weights=masked_weight
+    )
+    intersection_std_inv = np.sqrt(
+        np.cov(2 / masked_intersection, aweights=masked_weight)
+    )
+    logger.info(
         f"Intersection Average Inverse: {intersection_average_inv:.2f} ± {intersection_std_inv:.2f}"
     )
     value_tuple = (
-            intersection_average,
-            intersection_std,
-            intersection_average_inv,
-            intersection_std_inv,
-        )
+        intersection_average,
+        intersection_std,
+        intersection_average_inv,
+        intersection_std_inv,
+    )
+    mask_tuple = np.sum(np.asarray(masks)[finite_intersections]), np.sum(masked_weight)
     if detailed and return_neg_sum:
-        return value_tuple, neg_sum, intersection_points
+        return value_tuple, neg_sum, intersection_points, weight
     elif detailed:
-        return value_tuple
+        return value_tuple, mask_tuple
     if return_neg_sum:
         return neg_sum
 
-    return intersection_average, intersection_std
+    return (
+        intersection_average,
+        intersection_std,
+    )
 
 
 ################################################################################
@@ -315,6 +353,117 @@ def find_largest_blobs(
             neg_blob_masks[neg_labeled == old_idx] = new_idx
 
     return pos_blob_masks, neg_blob_masks
+
+
+def find_most_positive_blobs_np(
+    diffmap_np: np.ndarray,
+    *,
+    threshold: float,
+    minimum_size: int,
+    maximum_quantity: int,
+):
+    """
+    Finds the largest positive blobs in the difference map.
+
+    Returns a list of boolean masks, each mask corresponding to a blob, ordered by size (largest first).
+    """
+    max_val = np.max(diffmap_np)
+    pos_thresh = max_val * threshold
+    pos_mask = diffmap_np >= pos_thresh
+    structure = generate_binary_structure(3, 3)
+    pos_labeled, pos_num = label(pos_mask, structure=structure)
+    pos_blob_sizes = np.bincount(pos_labeled.ravel())
+    pos_blob_sizes[0] = 0  # background
+    pos_blob_sizes[pos_blob_sizes < minimum_size] = 0
+    pos_order = np.argsort(pos_blob_sizes)[::-1]
+    maximum_quantity = min(maximum_quantity, len(pos_order))
+    pos_order = pos_order[:maximum_quantity]
+    pos_blob_masks = []
+    for new_idx, old_idx in enumerate(pos_order):
+        if old_idx != 0 and pos_blob_sizes[old_idx] > 0:
+            logger.warning(f"Blob {new_idx}, inserting {old_idx}")
+            pos_blob_masks[pos_labeled == old_idx] = new_idx
+    return pos_blob_masks
+
+
+def find_most_positive_blobs_np(
+    diffmap_np: np.ndarray,
+    *,
+    threshold: float,
+    minimum_size: int,
+    maximum_quantity: int,
+):
+    max_val = np.max(diffmap_np)
+
+    # add assertions allowing only for thresh_pos and thresh_neg
+    # or threshold, not both
+    if threshold is not None:
+        thresh_pos = threshold
+
+    # Thresholds for positive and negative blobs
+    pos_thresh = max_val * thresh_pos
+
+    # Create masks for positive and negative blobs
+    pos_mask = diffmap_np >= pos_thresh
+
+    # Use 3D connectivity for labeling
+    structure = generate_binary_structure(3, 3)
+
+    # Label positive and negative blobs
+    pos_labeled, pos_num = label(pos_mask, structure=structure)
+
+    # Get sizes and sort order for positive blobs
+    pos_blob_sizes = np.bincount(pos_labeled.ravel())
+    pos_blob_sizes[0] = 0  # background
+    pos_blob_sizes[pos_blob_sizes < minimum_size] = 0  # filter out small blobs
+    pos_order = np.argsort(pos_blob_sizes)[::-1]  # largest first, skip 0
+
+    maximum_quantity = min(maximum_quantity, len(pos_order))
+    pos_order = pos_order[:maximum_quantity]
+
+    # Create masks for all positive blobs, ordered by size
+    pos_blob_masks = np.zeros_like(pos_labeled, dtype=int)
+
+    for new_idx, old_idx in enumerate(pos_order):
+        if old_idx != 0 and pos_blob_sizes[old_idx] > 0:
+            pos_blob_masks[pos_labeled == old_idx] = new_idx
+
+    return pos_blob_masks
+
+
+def find_largest_blobs2(
+    diffmap: rsmap.Map,
+    map_sampling: float,
+    *,
+    threshold: float = 0.5,
+    minimum_size: int = 3,
+    maximum_quantity: int = np.inf,
+    find_pos: bool = True,
+    find_neg: bool = True,
+):
+    diffmap_np = diffmap.to_3d_numpy_map(map_sampling=map_sampling)
+    if find_pos:
+        pos_blob_mask = find_most_positive_blobs_np(
+            diffmap_np,
+            threshold=threshold,
+            minimum_size=minimum_size,
+            maximum_quantity=maximum_quantity,
+        )
+    if find_neg:
+        neg_blob_mask = find_most_positive_blobs_np(
+            -diffmap_np,
+            threshold=threshold,
+            minimum_size=minimum_size,
+            maximum_quantity=maximum_quantity,
+        )
+    if find_pos and find_neg:
+        return pos_blob_mask, neg_blob_mask
+    elif find_pos:
+        return pos_blob_mask
+    elif find_neg:
+        return neg_blob_mask
+    else:
+        raise ValueError("At least one of find_pos or find_neg must be True.")
 
 
 ################################################################################
