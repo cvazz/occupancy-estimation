d3d5bab9633cc767c22823dd04a8deabbf2db4c0
Sebastian Bielfeldt <50203100+cvazz@users.noreply.github.com>
2026-03-13 09:10:01 +0100

    improving configs

--- configuration.py
+++ configuration.py
@@ -22,6 +22,35 @@ def minimal_masking_config():
             "dark_size_threshold": 0.,
             "exclude_large_occupancy_outliers": False,
     }
+def get_base_config():
+    return {
+        "masking": {
+            "sigma": 3,
+            "min_blob_size": 3,  # in A^3
+            "blocking_radius": 1.5,
+            "blocking_percentile": 95,
+            "exclude_solvent": True,
+            "dark_size_threshold": 0.1,
+            "exclude_positive_diffmap": True,
+            "exclude_large_occupancy_outliers": False,
+        },
+        "map_processing": {
+            "diffmap_type": "tv", # "kweighted", "tv", or "vanilla"
+            "dark_mean_correction": True,
+            "diffmap_mean_correction": True,
+            "diffmap_v2_correction": False,
+            "preprocessing": False,
+        },
+        "plot": {
+            "show_ignored_voxels": True,
+            "set_ylim": False,
+            "is_composite": False,
+            "std_cutoff": 3.0,
+            "solvent_density": 0.4,
+            "minimum_datapoints": 10,
+        },
+    }
+
 
 def get_file_config(
     dataloc_dark: str,
@@ -41,6 +70,7 @@ def get_file_config(
             "output_base_folder": outpath if outpath else load_homepath() +"tmp/diffmap_data/",
             "map_sampling": 3,
             "high_resolution_limit": high_resolution_limit,
+            "data_type": "triggered",  # "triggered" or "diffmap"
         },
         "input_files": {
             "map_dark": dataloc_dark,
@@ -50,32 +80,39 @@ def get_file_config(
             "columns_triggered": columns_triggered,
             "impose_dark_phases": True,
         },
-        "masking": {
-            "sigma": 3,
-            "min_blob_size": 3,  # in A^3
-            "blocking_radius": 1.5,
-            "blocking_percentile": 95,
-            "exclude_solvent": True,
-            "dark_size_threshold": 0.1,
-            "exclude_large_occupancy_outliers": False,
-        },
-        "map_processing": {
-            "diffmap_type": "tv", # "kweighted", "tv", or "vanilla"
-            "dark_mean_correction": True,
-            "diffmap_mean_correction": True,
-            "diffmap_v2_correction": False,
-        },
-        "plot": {
-            "show_ignored_voxels": True,
-            "set_ylim": False,
-            "is_composite": False,
-            "std_cutoff": 3.0,
-            "solvent_density": 0.4,
-            "minimum_datapoints": 10,
-        },
-    }
+    } | get_base_config()
 
     output_folder = config["general"]["output_base_folder"] + "/" 
     config["general"]["output_folder"] = output_folder
     config["general"]["pdbloc_dark"] = pdbloc_dark
+    return config
+
+def get_file_config_diff_only(
+    dataloc_dark: str,
+    dataloc_diff: str,
+    pdbloc_dark: str,
+    columns_dark: dict,
+    columns_diff: dict,
+    high_resolution_limit: float = 0.1,
+    name_machine: str = "unnamed_experiment",
+    name_human: str | None = None,
+    outpath: str | None = None,
+):
+    config =  {
+        "general": {
+            "name_human": name_human if name_human else name_machine,
+            "name_machine": name_machine,
+            "output_base_folder": outpath if outpath else load_homepath() +"tmp/diffmap_data/",
+            "map_sampling": 3,
+            "high_resolution_limit": high_resolution_limit,
+            "data_type": "diff",  # "triggered" or "diffmap"
+        },
+        "input_files": {
+            "map_dark": dataloc_dark,
+            "map_diff": dataloc_diff,
+            "pdb_dark": pdbloc_dark,
+            "columns_dark": columns_dark,
+            "columns_diff": columns_diff,
+        },
+    } | get_base_config()
     return config
